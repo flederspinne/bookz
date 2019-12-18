544e7a8489fa5510a336da09f7f3e433ad4b95ea
KseniaTaranova <Ksenia.Taranova@center2m.ru>
2019-12-18 16:51:01 +0300

    Выполнена операция eject, настройки Webpack сконфигурированы таким образом, чтобы во время сборки протягивался файл с глобальными переменными.

--- frontend/src/common/components/Button/Button.js
+++ frontend/src/common/components/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import './Button.scss'
+import s from './Button.module.scss'
 
 
 const Button = (props) => {
@@ -9,7 +9,7 @@ const Button = (props) => {
         children
     } = props
 
-    return <button className="button" onClick={onClick}>{children}</button>
+    return <button className={s.button} onClick={onClick}>{children}</button>
 }
 
 export default Button
