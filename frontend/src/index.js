310036ea7705c2161b1310ee78693f42e976f8f7
KseniaTaranova <Ksenia.Taranova@center2m.ru>
2019-12-20 09:49:01 +0300

    Внедрены шрифты.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './common/styles/fonts.scss'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
