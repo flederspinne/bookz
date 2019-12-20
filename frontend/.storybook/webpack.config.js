a15972fd901aa07e1d65c0b810fcc608a83abd1a
KseniaTaranova <Ksenia.Taranova@center2m.ru>
2019-12-20 09:08:47 +0300

    Сконфигурированы настройки Webpack для Storybook.

--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -1,18 +1,26 @@
-const path = require('path');
-
-// Export a function. Accept the base config as the only param.
-module.exports = async ({ config, mode }) => {
-    // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../'),
-    });
-
-    // Return the altered config
-    return config;
-};
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: [
+                    require.resolve('style-loader'),
+                    {
+                        loader: require.resolve('css-loader'),
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                        },
+                    },
+                    {
+                        loader: require.resolve('sass-loader'),
+                        options: {
+                            sourceMap: true,
+                            prependData: '@import "src/common/styles/index";',
+                        }
+                    }
+                ],
+            },
+        ],
+    },
+}
