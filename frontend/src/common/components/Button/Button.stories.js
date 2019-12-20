9d8f9a2672a439f42e2331be59103aef2e6b33c9
KseniaTaranova <Ksenia.Taranova@center2m.ru>
2019-12-20 10:01:12 +0300

    Доработан компонент Кнопка.

--- frontend/src/common/components/Button/Button.stories.js
+++ frontend/src/common/components/Button/Button.stories.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import Button from './Button';
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Button from './Button'
 
-export default {
-  component: Button,
-  title: 'Button',
-};
 
-export const text = () => <Button onClick={action('clicked')}>lol kek</Button>;
+storiesOf('Button', module)
+    .add('Default', () => <Button onClick={action('clicked')}>Button</Button>)
 
