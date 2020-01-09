af571201b3ad0834665c335d7a8d7f5d224849c3
KseniaTaranova <Ksenia.Taranova@center2m.ru>
2020-01-09 15:41:38 +0300

    Начало разработки панели входа в систему.

--- frontend/src/common/components/Header/Header.js
+++ frontend/src/common/components/Header/Header.js
@@ -1,28 +1,72 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
 
 import Box from '../Box/Box'
 import Link from '../Link/Link'
 import Logo from '../Logo/Logo'
+import Panel from '../Panel/Panel'
+import Input from '../Input/Input'
+import Button from '../Button/Button'
+
+import api from '../../helpers/api'
 
 import s from './Header.module.scss'
 
 
 const Header = () => {
-    return (
-        <div className={s.header}>
-            <Box mt="xxs" ml="md" className={s.wrapper}>
-                <Logo />
 
-                <div className={s.logInWrapper}>
-                    <Box mt="xsm" mr="sm">
-                        <Link>Войти</Link>
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    const submit = async () => {
+        let response = await fetch(api.authLogin, {
+            method: 'POST',
+            mode: 'cors',
+            withCredentials: true,
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+
+        let result = await response.json()
+        console.log(result)
+    }
+
+    return (
+        <Fragment>
+            <div className={s.header}>
+                <Box mt="xxs" ml="md" className={s.wrapper}>
+                    <Logo />
+
+                    <div className={s.logInWrapper}>
+                        <Box mt="xsm" mr="sm">
+                            <Link>Войти</Link>
+                        </Box>
+                        <Box mt="xsm" mr="sm">
+                            <Link>Зарегистрироваться</Link>
+                        </Box>
+                    </div>
+                </Box>
+            </div>
+            <Box mt="lg" mr="xlg" className={s.panelWrapper}>
+                <Panel
+                    title="Вход в систему"
+                    className={s.panel}
+                >
+                    <Input label="Логин" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <Box mt="xs">
+                        <Input label="Пароль" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </Box>
-                    <Box mt="xsm" mr="sm">
-                        <Link>Зарегистрироваться</Link>
+                    <Box mt="xs" className={s.panelButton}>
+                        <Button onClick={submit}>Вход</Button>
                     </Box>
-                </div>
+                </Panel>
             </Box>
-        </div>
+        </Fragment>
     )
 }
 
