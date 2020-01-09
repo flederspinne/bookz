f8492740fb236eac392ca55984eeb1d761633c3b
KseniaTaranova <Ksenia.Taranova@center2m.ru>
2020-01-09 14:32:07 +0300

    Выделен компонент для ссылок.

--- frontend/src/common/components/Header/Header.js
+++ frontend/src/common/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Box from '../Box/Box'
+import Link from '../Link/Link'
 import Logo from '../Logo/Logo'
 
 import s from './Header.module.scss'
@@ -9,8 +10,17 @@ import s from './Header.module.scss'
 const Header = () => {
     return (
         <div className={s.header}>
-            <Box mt="xxs" ml="md">
+            <Box mt="xxs" ml="md" className={s.wrapper}>
                 <Logo />
+
+                <div className={s.logInWrapper}>
+                    <Box mt="xsm" mr="sm">
+                        <Link>Войти</Link>
+                    </Box>
+                    <Box mt="xsm" mr="sm">
+                        <Link>Зарегистрироваться</Link>
+                    </Box>
+                </div>
             </Box>
         </div>
     )
