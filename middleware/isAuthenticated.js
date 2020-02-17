26b52490c17e754126c096b8dd5f94b9531c3c2d
KseniaTaranova <Ksenia.Taranova@center2m.ru>
2020-02-17 13:24:27 +0300

    Revert "Бэкенд: загрузка аватара."
    This reverts commit b81ad10a

--- middleware/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-isAuth = (req, res, next) => {
-    if(!req.isAuthenticated())
-        return res.status(403).end()
-
-    next()
-}
-
-module.exports = isAuth
--- models/Avatar.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-
-const Avatar = new Schema({
-    path: String,
-});
-
-module.exports = mongoose.model('Avatar', Avatar);
